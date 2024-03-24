6e1a786531879194d2d7aeaec6e1e48bcabbb2fc
Clément Carré <clement.carre@bionomeex.com>
2024-03-24 08:44:30 +0100

    Add usage example

--- usage.py
+++ usage.py
@@ -12,5 +12,13 @@ class DenseLayerTester(KATE.BaseLayerTester):
     expected_shape = [None, 64]
 
 
+class LambdaLayerTester(KATE.BaseLayerTester):
+    layer_class = tf.keras.layers.Lambda
+    layer_args = {"function": lambda x: x + 1}
+    input_shape = [None, 2, 5]
+    expected_shape = [None, 2, 5]
+    known_values = {"input": [tf.ones((2, 5))], "output": [tf.ones((2, 5)) * 2]}
+
+
 if __name__ == "__main__":
     unittest.main()
